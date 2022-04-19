ea9ead2c0bd4633e23e590315ddcbb957e6dbc7f
YuriRoque <yrbenvenutti@gmail.com>
2022-04-19 14:13:55 -0400

    express initial config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
 import connectDB from './config/DB.js';
 
+dotenv.config();
+
+const app = express();
+
 await connectDB();
+
+const SERVER_PORT = process.env.SERVER_PORT;
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.listen(SERVER_PORT, () =>
+  console.log(`Server is running on PORT ${SERVER_PORT}`),
+);
