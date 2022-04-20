ea1cba272e78a1d0d9e9978924052b0a5a57231d
Yuri Roque Benvenutti <yrbenvenutti@gmail.com>
2022-04-20 19:22:31 -0400

    Using ScheduleRouter in express middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 
 import connectDB from './config/DB.js';
+import ScheduleRouter from './router/SchedulerRouter.js';
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const SERVER_PORT = process.env.SERVER_PORT;
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(ScheduleRouter);
 
 app.listen(SERVER_PORT, () =>
   console.log(`Server is running on PORT ${SERVER_PORT}`),
